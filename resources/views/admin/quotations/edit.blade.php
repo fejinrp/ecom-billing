56d8260c7d7b3d6bcce94afe724d16369a5c8a31
Fejin RP <fejinrp@gmail.com>
2026-05-24 23:28:16 +0530

    fix: update quotation product input trigger from change to input event

--- resources/views/admin/quotations/edit.blade.php
+++ resources/views/admin/quotations/edit.blade.php
@@ -222,7 +222,7 @@
                                                        x-model="item.productName"
                                                        required
                                                        list="quotation-product-suggestions"
-                                                       @change="onCustomProductInput(index)"
+                                                       @input="onCustomProductInput(index)"
                                                        placeholder="Type custom or select suggestion..."
                                                        class="w-full px-3 py-2 bg-slate-950 border border-slate-850 rounded-lg text-slate-200 text-sm focus:outline-none focus:border-indigo-500 uppercase">
                                             </div>
